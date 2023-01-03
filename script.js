e12dce6539cdacd36173de4682b8a1f2389a99e3
Kay Beckmann <info@beckmann-md.de>
2023-01-03 20:59:44 +0100

    Dialog für Resetbutton

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let rows = new Array;
 let gamefield = new Array;
 let player = true;
+let finished = false;
 
 function initailsation(){
   for(let row=0; row<3;row++){
     rows=[];
     for(let column=0; column<3; column++){
-      rows.push(0);
+      rows.push(10);
     }
     gamefield.push(rows);
   }
@@ -15,27 +16,30 @@ function initailsation(){
 function reset(){
   for(let row=0; row<gamefield.length; row++){
     for(let column=0; column<rows.length; column++){
-      gamefield[row][column] = 0;
-      console.log(gamefield[row[column]]);
+      let id = String(row) + String(column);
+      gamefield[row][column] = 10;
+      document.getElementById(id).innerHTML = "";
     }
   }
-  console.log(gamefield);
+  document.getElementById("dialog").classList.add("d-none");
+  player = true;
+  changePlayerSign();
 }
 
 function fillShape(row, column){
   let field = document.getElementById(String(row)+String(column))
 
-  if(gamefield[Number(row)][Number(column)] == 0){
-    let sign = getSign();
-    gamefield[Number(row)][Number(column)] = sign;
-    field.innerHTML = sign;
+  if(gamefield[Number(row)][Number(column)] == 10){
+    field.innerHTML = getSign(row, column);
+    checkForWin();
     console.log(gamefield);
   }else{
     console.log("ERROR");
   }
 }
 
-function getSign(){
+function getSign(row, column){
+  gamefield[Number(row)][Number(column)] = player;
   if(player){
     player = false;
     changePlayerSign();
@@ -56,5 +60,12 @@ function changePlayerSign(){
   }else{
     circle.classList.add("d-none");
     cross.classList.remove("d-none");
+  }
+}
+
+function checkForWin(){
+  if(gamefield[0][0] == gamefield[0][1] == gamefield[0][2]){
+    console.log("GEWONNEN");
+    document.getElementById("dialog").classList.remove("d-none");
   }
 }
